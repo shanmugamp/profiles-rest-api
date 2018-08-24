74e0071e61d69e46e748632fb783b7dd24b2a1a3
Shanmugam P <shanmugamp26@gmail.com>
2018-08-24 22:16:56 +0530

    Test Commit

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles_api',
-    'restframework',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
