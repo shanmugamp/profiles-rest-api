87054c9c681ec2ba327b1267f8e01c80542ae987
Shanmugam P <shanmugamp26@gmail.com>
2018-08-25 15:40:07 +0530

    URL mapping in Django

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloAPIView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """ Returns a list of APIView features. """
+
+        an_apiview = ['uses HTTP mehods as function (get, post, patch, put, delete)','Traditional django view','Third value',
+        'It is mapped manually to URLs'
+        ]
+
+        return Response({'message':'Hello', 'an_apiview': an_apiview})
